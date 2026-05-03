9c04ed9bf56ab44a6bd829913972603d57c278fd
DESKTOP-26A9125\PC <raymondgachoki3@gmail>
2026-05-03 09:50:34 +0300

    Fix database config to use environment variable

--- app/config.py
+++ app/config.py
@@ -1,10 +1,12 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'smartedge-secret-key-2026')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'smartedge-jwt-secret-2026')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/smartedge'
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL',
+        'mysql+pymysql://root:@localhost/smartedge'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)gi
